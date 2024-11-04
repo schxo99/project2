ef3fd67d2af59e292c32eb9179e483764b69c42c
Sangha <shhwangofficial@gmail.com>
2024-11-04 21:06:19 +0900

    feat: create DB pool

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./src/config/.env" });
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  connectionLimit: process.env.DB_CONNECTION,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pool;

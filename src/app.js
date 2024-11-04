476b40dc9a1e61adbe5a4066fd1a2d6ddac31bed
Sangha <shhwangofficial@gmail.com>
2024-11-04 21:24:55 +0900

    refactor: set and apply prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express();
 
-dotenv.config({ path: "./src/config/.env" });
+dotenv.config({ path: './src/config/.env' });
 
 app.listen(process.env.PORT);
